a1bdcb8df7746a05d4df94a9bef7833a58b137a8
ライヴライブ 開発 <develop@livlib.co.jp>
2018-12-27 09:33:11 +0900

    Commit on Dec/27/2018

---
+++ proj01/home/views.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def index(request):
+    message = ""
+    message += 'home からのメッセージです。<br />'
+    message += str(request.user.id) + '&nbsp;&nbsp;'
+    message += request.user.username + '<p />'
+    dd = {
+        'hour': datetime.now().hour,
+        'minute': datetime.now().minute,
+        'message': message,
+    }
+    return render(request, 'home/home.html', dd)
+
